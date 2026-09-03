41523bc3c4e73af7b9fa12c9742ef417f512d72b
elias3-lab <sassielias3@gmail.com>
2026-09-03 13:36:40 +0100

    feat: add core project state package

---
+++ core/__init__.py
@@ -0,0 +1,5 @@
+"""Core project orchestration primitives."""
+
+from .project_state import ProjectState, SceneState, ProjectStatus, SceneStatus
+
+__all__ = ["ProjectState", "SceneState", "ProjectStatus", "SceneStatus"]
