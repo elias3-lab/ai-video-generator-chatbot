4251d192ba0bc6c95e2b628a8ddf1cdc16ad7dea
elias3-lab <sassielias3@gmail.com>
2026-09-03 14:15:53 +0100

    feat: expose smart scene routing and provider fallback

--- core/__init__.py
+++ core/__init__.py
@@ -3,7 +3,9 @@
 from .media_router import MediaMode, RouteDecision, SmartMediaRouter
 from .orchestrator import PipelineOrchestrator, SceneResult
 from .project_state import ProjectState, SceneState, ProjectStatus, SceneStatus
+from .scene_decision import SceneContext, SceneDecision, SceneMediaMode, decide_scene_media
 from .scene_planner import ScenePlan, plan_scenes, validate_target_duration
+from .provider_fallback import Attempt, FallbackResult, AllProvidersFailed, run_with_fallback
 
 __all__ = [
     "MediaMode",
@@ -18,4 +20,12 @@ __all__ = [
     "ScenePlan",
     "plan_scenes",
     "validate_target_duration",
+    "SceneContext",
+    "SceneDecision",
+    "SceneMediaMode",
+    "decide_scene_media",
+    "Attempt",
+    "FallbackResult",
+    "AllProvidersFailed",
+    "run_with_fallback",
 ]
