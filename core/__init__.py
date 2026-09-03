2d2530cd7bcf5b8dec322f7f5761b8e982914282
elias3-lab <sassielias3@gmail.com>
2026-09-03 13:40:42 +0100

    chore: export core pipeline primitives

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,21 @@
 """Core project orchestration primitives."""
 
+from .media_router import MediaMode, RouteDecision, SmartMediaRouter
+from .orchestrator import PipelineOrchestrator, SceneResult
 from .project_state import ProjectState, SceneState, ProjectStatus, SceneStatus
+from .scene_planner import ScenePlan, plan_scenes, validate_target_duration
 
-__all__ = ["ProjectState", "SceneState", "ProjectStatus", "SceneStatus"]
+__all__ = [
+    "MediaMode",
+    "RouteDecision",
+    "SmartMediaRouter",
+    "PipelineOrchestrator",
+    "SceneResult",
+    "ProjectState",
+    "SceneState",
+    "ProjectStatus",
+    "SceneStatus",
+    "ScenePlan",
+    "plan_scenes",
+    "validate_target_duration",
+]
